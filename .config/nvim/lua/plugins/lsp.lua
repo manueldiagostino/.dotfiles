d4b5341b28289fbcf79ede48aed1e61cb107e24a
manueldiagostino <diagostinomanuel@gmail.com>
2024-02-18 12:27:18 +0100

    aggiunta configurazione per lsp

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
+    opts = {
+      automatic_installation = true,
+      ensure_installed = {
+        "clangd",
+        "jdtls",
+        "ltex-ls",
+        "pyre",
+      },
+    },
+  },
+}
